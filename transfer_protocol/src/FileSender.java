bf3e1991315eceb630feb8d053cb387acff68b4b
Your Name <kkaiser@hm,edu>
2018-12-08 10:11:50 +0100

    Datenstrom wird zusammen mit Checksumme übertragen

--- transfer_protocol/src/FileSender.java
+++ transfer_protocol/src/FileSender.java
@@ -1,68 +1,100 @@
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.stream.Stream;
+import java.util.zip.CRC32;
 
 public class FileSender {
-    private  final String filename;
-    private  final String adress;
     private static final int PORT = 90;
     private static final int BUFFER_LENGTH = 1024;
-    private static final int TIMEOUT = 30000;
+    private static final int TIMEOUT = 15000;
+    private static final double PROBABILITY_PACKET_FAILURE = 0.05;
+
+    private final String filename;
+    private final String adress;
+    private final CRC32 crc = new CRC32();
+    private final Transition[][] transition;
+    private final FileInputStream fileInputStream;
+    private final DatagramSocket sendSocket = new DatagramSocket();
+    private final byte[] fileData = new byte[1024];
+    private State currentState;
+    private int read;
+    private byte alternatingBit;
 
 
-    public FileSender(String filename, String adress){
-        this.filename =filename ;
+
+    public FileSender(String filename, String adress) throws FileNotFoundException, SocketException {
+        this.filename = filename;
         this.adress = adress;
 
         currentState = State.WAIT_FOR_CALL_FROM_ABOVE;
-        // define all valid state transitions for our state machine
-        // (undefined transitions will be ignored)
-        transition = new Transition[State.values().length] [Message.values().length];
-        transition[State.WAIT_FOR_CALL_FROM_ABOVE.ordinal()] [Message.GOT_CALL_FROM_ABOVE.ordinal()] = new SendNewPackage();
-        transition[State.WAIT_FOR_ACK.ordinal()] [Message.TIMEOUT.ordinal()] = new ResendPackage();
-        transition[State.WAIT_FOR_ACK.ordinal()] [Message.RECEIVED_ACK.ordinal()] = new ReceiveAck();
-        System.out.println("INFO Sender constructed, current state: "+currentState);
+
+        transition = new Transition[State.values().length][Message.values().length];
+        transition[State.WAIT_FOR_CALL_FROM_ABOVE.ordinal()][Message.GOT_CALL_FROM_ABOVE.ordinal()] = new SendNewPackage();
+        transition[State.WAIT_FOR_ACK_0.ordinal()][Message.TIMEOUT.ordinal()] = new ResendPackage();
+        transition[State.WAIT_FOR_ACK_0.ordinal()][Message.RECEIVED_ACK_0.ordinal()] = new ReceiveAck();
+        transition[State.WAIT_FOR_ACK_1.ordinal()][Message.TIMEOUT.ordinal()] = new ResendPackage();
+        transition[State.WAIT_FOR_ACK_1.ordinal()][Message.RECEIVED_ACK_1.ordinal()] = new ReceiveAck();
+        System.out.println("INFO Sender constructed, current state: " + currentState);
+
+        fileInputStream = new FileInputStream(filename);
     }
 
     public static void main(String[] args) throws IOException {
-        FileSender fileSender = new FileSender(args[0],args[1]);
-        fileSender.processMsg(Message.GOT_CALL_FROM_ABOVE);
-    }
+        FileSender fileSender = new FileSender(args[0], args[1]);
+        fileSender.processMessage(Message.GOT_CALL_FROM_ABOVE);
 
-
-    // all states for this FSM
-    enum State {
-        WAIT_FOR_CALL_FROM_ABOVE, WAIT_FOR_ACK
-    };
-    // all messages/conditions which can occur
-    enum Message {
-        GOT_CALL_FROM_ABOVE, TIMEOUT, RECEIVED_ACK
-    }
-    // current state of the FSM
-    private State currentState;
-    // 2D array defining all transitions that can occur
-    private Transition[][] transition;
-
-
-    public void sendPacket() throws IOException {
-        try(FileInputStream fileInputStream = new FileInputStream("C:\\Users\\Kristina\\Desktop\\Studium\\Netzwerke\\Netzwerk1\\transfer_protocol\\FileSenderTest.txt");
-            DatagramSocket sendSocket = new DatagramSocket();) {
-            InetAddress IPAddress = InetAddress.getByName(adress);
-            byte[] fileData = new byte[1024];
-            int read = fileInputStream.read(fileData);
-
-
-            while (read != -1) {
-                DatagramPacket datagramPacket = new DatagramPacket(fileData, BUFFER_LENGTH, IPAddress, PORT);
-                sendSocket.send(datagramPacket);
-                read = fileInputStream.read(fileData);
-                System.out.println("read" + read);
+        while (fileSender.getRead() != -1) {
+            if (fileSender.getCurrentState() == State.WAIT_FOR_CALL_FROM_ABOVE) {
+                fileSender.processMessage(Message.GOT_CALL_FROM_ABOVE);
             }
+            else if (fileSender.getCurrentState() == State.WAIT_FOR_ACK_0) {
+                    fileSender.waitForAck((byte) 0);
+            }
+
         }
-        processMsg(Message.RECEIVED_ACK);
+        System.out.println("CRC" + fileSender.getCrc().getValue());
     }
 
 
-    public void waitForAck() throws IOException {
+    public void sendPacket(boolean packetIsNew) throws IOException {
+        InetAddress IPAddress = InetAddress.getByName(adress);
+
+        /**send new data, if ack for old data was received**/
+        if (packetIsNew) {
+            read = fileInputStream.read(fileData,0, 1016);
+            crc.update(fileData);
+
+            /**put crc long in an byte array with length 8 and send the checksum with data as the last 8 bytes (1016 - 1032) in the array file data**/
+            byte[] crcBytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(crc.getValue()).array();
+            Stream.iterate(0,i-> i+1).limit(8).forEach(i -> fileData[1016+i] = crcBytes[i]);
+            System.out.println("crc single packets" + crc.getValue());
+            System.out.println("crcBytes.length" + crcBytes.length);
+
+
+
+        }
+
+        /**send original packet or packet with a bit failure**/
+        DatagramPacket datagramPacket;
+        if (!decideToSendPacketFailure()) {
+             datagramPacket = new DatagramPacket(fileData, BUFFER_LENGTH, IPAddress, PORT);
+        }
+        else {
+             datagramPacket = new DatagramPacket(generatePacketFailure(fileData), BUFFER_LENGTH, IPAddress, PORT);
+            System.out.println("generated packet failure");
+        }
+        sendSocket.send(datagramPacket);
+        crc.reset();
+        //read = fileInputStream.read(fileData);
+
+
+
+    currentState =State.WAIT_FOR_ACK_0;
+}
+
+
+    public void waitForAck(byte alternatingBit) throws IOException {
         System.out.println("Waiting for ACK");
         try(DatagramSocket receiveSocket = new DatagramSocket(100);) {
             byte[] ackData = new byte[1024];
@@ -71,32 +103,57 @@ public class FileSender {
             receiveSocket.setSoTimeout(TIMEOUT);
             while(!outOfTime  && !received) {
                 try {
-                    System.out.println("received");
                     DatagramPacket datagramPacket = new DatagramPacket(ackData, BUFFER_LENGTH);
                     receiveSocket.receive(datagramPacket);
                     String answer = new String(ackData);
-                    System.out.println("answer" + answer);
-                    if (answer.equals("ACK")) {
-                        received = true;
-                        processMsg(Message.RECEIVED_ACK);
 
+                        if (alternatingBit == 1 && answer.contains("ACK1") || alternatingBit== 0 && answer.contains("ACK0")) {
+                        received = true;
+                        System.out.println("received ack" + answer);
+                        currentState = State.WAIT_FOR_CALL_FROM_ABOVE;
 
                     }
                 } catch (SocketTimeoutException exception) {
                     outOfTime = true;
-                    System.out.println("Timeout");
-                    processMsg(Message.TIMEOUT);
+                    System.out.println("timeout");
+                    processMessage(Message.TIMEOUT);
                 }
             }
             }
         }
-   // }
+
+
+        /**decides if a bit failure should be generated using a given probability**/
+        public boolean decideToSendPacketFailure() {
+            return Math.random() <= PROBABILITY_PACKET_FAILURE;
+        }
+
+
+        /**generates a bit failure**/
+        public byte[] generatePacketFailure(byte[] originalPacket) {
+        //TODO Bitfehler an einer zufaelligen Stelle erzeugen
+        final int failureBytePosition = 2;
+        final int flipPosition = 5;
+        byte[] failurePacket = originalPacket.clone();
+        byte failureByte = originalPacket[failureBytePosition];
+            failureByte =  (byte) (failureByte ^ (1 << flipPosition));
+            failurePacket[failureByte] = failureByte;
+        return failurePacket;
+        }
+
+   enum State {
+       WAIT_FOR_CALL_FROM_ABOVE, WAIT_FOR_ACK_0, WAIT_FOR_ACK_1
+   };
+
+    enum Message {
+        GOT_CALL_FROM_ABOVE, TIMEOUT, RECEIVED_ACK_0, RECEIVED_ACK_1
+    }
 
     /**
      * Process a message (a condition has occurred).
      * @param input Message or condition that has occurred.
      */
-    public void processMsg(Message input) throws IOException {
+    public void processMessage(Message input) throws IOException {
         System.out.println("INFO Received "+input+" in state "+currentState);
         Transition trans = transition[currentState.ordinal()][input.ordinal()];
         if(trans != null){
@@ -117,27 +174,38 @@ public class FileSender {
     class SendNewPackage extends Transition {
         @Override
         public State execute(Message input) throws IOException {
-            sendPacket();
-            waitForAck();
-            return State.WAIT_FOR_ACK;
+            System.out.println("execute send packet");
+            sendPacket(true);
+            return State.WAIT_FOR_ACK_0;
         }
     }
-
 
     class ResendPackage extends Transition {
         @Override
         public State execute(Message input) throws IOException {
-            sendPacket();
-            return State.WAIT_FOR_ACK;
+            System.out.println("execute resend package");
+            sendPacket(false);
+            return State.WAIT_FOR_ACK_0;
         }
     }
-
 
     class ReceiveAck extends Transition {
         @Override
         public State execute(Message input) {
+            System.out.println("Wait for call from above again.");
             return State.WAIT_FOR_CALL_FROM_ABOVE;
         }
     }
 
+    public State getCurrentState() {
+        return currentState;
+    }
+
+    public int getRead() {
+        return read;
+    }
+
+    public CRC32 getCrc() {
+        return crc;
+    }
 }
--- transfer_protocol/src/TestReceiver.java
+++ transfer_protocol/src/TestReceiver.java
@@ -1,35 +1,57 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketTimeoutException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
 
 public class TestReceiver {
-    public static void main(String[] args) throws IOException {
-        TestReceiver testReceiver = new TestReceiver();
-       testReceiver.receivePacketsUDP();
+    private final CRC32 crc = new CRC32();
+    DatagramSocket serverSocket = new DatagramSocket(90);
+    static boolean outOfTime = false;
 
-       testReceiver.sendPacket();
-        ;
+    public TestReceiver() throws SocketException {
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        TestReceiver testReceiver = new TestReceiver();
+        int i = 0;
+        while(! outOfTime) {
+      /*      if (i % 100 == 0) {
+            Thread.sleep(20000);
+            }*/
+            testReceiver.receivePacketsUDP();
+            testReceiver.sendPacket();
+            i++;
+        }
+        System.out.println("crc" + testReceiver.crc.getValue());
     }
 
     private  long receivePacketsUDP() throws IOException {
-        System.out.println("receive packets udp");
-        boolean outOfTime = false;
+
         byte[] receiveData = new byte[1024];
-        try(DatagramSocket serverSocket = new DatagramSocket(90);) {
+
             serverSocket.setSoTimeout(10000);
-            while (!outOfTime ) {
+
                 try {
                     DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                     serverSocket.receive(receivePacket);
+                    crc.update(receiveData, 0 , 1016);
+                    byte[] crcBytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(crc.getValue()).array();
+                    ByteBuffer buffer = ByteBuffer.wrap(crcBytes, 0,Long.SIZE / Byte.SIZE);
+                    buffer.put(receiveData, 1016, 8);
+                    buffer.rewind();
+                    long receivedCrc = buffer.getLong();
+
+                    System.out.println("received crc" + receivedCrc);
+                    boolean isRightChecksum = receivedCrc == crc.getValue();
+                    System.out.println("CRC is" + isRightChecksum);
                     System.out.println(new String(receiveData));
+                    crc.reset();
                 } catch (SocketTimeoutException exception) {
                     outOfTime = true;
                 }
-            }
-        }
+
+
         System.out.println("ended receive packets");
         return System.currentTimeMillis();
     }
